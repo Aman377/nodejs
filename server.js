d20ef59c0d14f29d144c724a14c8b5155cf1e17b
Aman <ansari.aman020404@gmail.com>
2024-04-27 02:43:50 +0530

    first api and mongodb setup

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const mongoose = require('mongoose');
 require("dotenv").config();
+const userRoutes = require('./src/routes/userRoutes')
 
 const app = express();
 const port = process.env.PORT;
+mongoose.connect(process.env.MONGO_URL);
+const db = mongoose.connection;
 
-app.get("/",(req,res) => {
-    res.send("Node js project connected");
-})
+// Connect mongodb
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log(`Connected to MongoDB`);
+});
+db.on('error', (error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Node.js project connected");
+});
+
+// User api routes
+app.use('/api', userRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
-})
+});
---
+++ src/controllers/userController.js
@@ -0,0 +1,10 @@
+const User = require('../models/userModel')
+
+exports.UserCreate = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(200).json({ "status": 200, "message": "User added successfully", "data": user })
+    } catch (err) {
+        res.status(500).json({ message: "Internal server error", error: err.message })
+    }
+}
