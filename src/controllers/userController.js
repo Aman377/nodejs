8db9eb1dec829041cdfa3e3a761114da11b67b21
Aman <ansari.aman020404@gmail.com>
2024-04-27 14:24:21 +0530

    wip

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,13 @@
-const User = require('../models/userModel')
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
 exports.UserCreate = async (req, res) => {
     try {
-        const user = await User.create(req.body);
+        const { name, email, password, mobileNo } = req.body
+        const hashPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name, email, password: hashPassword, mobileNo
+        });
         res.status(200).json({ "status": 200, "message": "User added successfully", "data": user })
     } catch (err) {
         res.status(500).json({ message: "Internal server error", error: err.message })
